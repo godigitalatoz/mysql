6988dafa83be70537918eb86efd2a9d0d9d3576c
godigitalatoz <134773995+godigitalatoz@users.noreply.github.com>
2023-08-07 18:52:08 +0500

    Add files via upload

---
+++ exercise_11.sql
@@ -0,0 +1,14 @@
+SELECT 
+	o.order_id,
+    o.order_date,
+    c.first_name,
+    sh.name AS shipper,
+    os.name AS status
+FROM orders o
+JOIN customers c
+	ON o.customer_id = c.customer_id
+LEFT JOIN shippers sh
+	ON o.shipper_id = sh.shipper_id
+LEFT JOIN order_statuses os
+	ON o.status = os.order_status_id
+ORDER BY status
